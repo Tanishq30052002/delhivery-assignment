615e5d30dce9044cfb76c9880b03c4a0fd45ddf4
Tanishq Chaudhary <tanishqchaudhary101010@gmail.com>
2025-10-24 03:04:10 +0000

    refactor the nav launch
    Signed-off-by: Tanishq Chaudhary <tanishqchaudhary101010@gmail.com>

--- multi-robot-navigation/launch/navigation.launch.py
+++ multi-robot-navigation/launch/navigation.launch.py
@@ -17,123 +17,114 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from nav2_common.launch import RewrittenYaml
 
 
 def generate_launch_description():
-    # Get the launch directory
-    bringup_dir = get_package_share_directory('multi-robot-navigation')
-
-    ns = LaunchConfiguration('namespace')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    autostart = LaunchConfiguration('autostart')
-    params_file = LaunchConfiguration('params_file')
-    default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
-    map_subscribe_transient_local = LaunchConfiguration('map_subscribe_transient_local')
+    robots_nav = {
+        'tug_1': 'nav2_params_tug_1.yaml',
+        # 'tug_2': 'nav2_params_tug_2.yaml',
+        # 'picker_1': 'nav2_params_picker_1.yaml',
+        # 'picker_2': 'nav2_params_picker_2.yaml',
+    }
 
     lifecycle_nodes = ['controller_server',
-                       'planner_server',
-                       'recoveries_server',
-                       'bt_navigator',
-                       'waypoint_follower'
-                       ]
+                        'planner_server',
+                        'recoveries_server',
+                        'bt_navigator',
+                        'waypoint_follower'
+                        ]
+
+
+    default_bt_xml_filename=os.path.join(
+                get_package_share_directory('nav2_bt_navigator'),
+                'behavior_trees',
+                'navigate_w_replanning_and_recovery.xml'
+    )
 
     # Create our own temporary YAML files that include substitutions
     param_substitutions = {
-        'use_sim_time': use_sim_time,
+        'use_sim_time': 'true',
         'default_bt_xml_filename': default_bt_xml_filename,
-        'autostart': autostart,
-        'map_subscribe_transient_local': map_subscribe_transient_local}
+        'autostart': 'true',
+        'map_subscribe_transient_local': 'false'
+    }
 
-    configured_params = RewrittenYaml(
-            source_file=params_file,
+    nav_nodes = []
+    lifecycle_manager_nodes = []
+    for ns, config_file in robots_nav.items():
+        config_file_path = os.path.join(
+            get_package_share_directory('multi-robot-navigation'),
+            'config',
+            config_file
+        )
+        configured_params = RewrittenYaml(
+            source_file=config_file_path,
             root_key=ns,
             param_rewrites=param_substitutions,
             convert_types=True)
+        nav_nodes.append(
+            Node(
+                package='nav2_controller',
+                executable='controller_server',
+                output='screen',
+                namespace=ns,
+                parameters=[configured_params]
+            )
+        )
+        nav_nodes.append(
+            Node(
+                package='nav2_planner',
+                executable='planner_server',
+                name='planner_server',
+                output='screen',
+                namespace=ns,
+                parameters=[configured_params]
+            )
+        )
+        nav_nodes.append(
+            Node(
+                package='nav2_recoveries',
+                executable='recoveries_server',
+                name='recoveries_server',
+                output='screen',
+                namespace=ns,
+                parameters=[configured_params]
+            )
+        )
+        nav_nodes.append(
+            Node(
+                package='nav2_bt_navigator',
+                executable='bt_navigator',
+                name='bt_navigator',
+                output='screen',
+                namespace=ns,
+                parameters=[configured_params]
+            )
+        )
+        nav_nodes.append(
+            Node(
+                package='nav2_waypoint_follower',
+                executable='waypoint_follower',
+                name='waypoint_follower',
+                output='screen',
+                namespace=ns,
+                parameters=[configured_params]
+            )
+        )
 
-    return LaunchDescription([
-        # Set env var to print messages to stdout immediately
-        SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'),
+        lifecycle_manager_nodes.append(Node(
+                                    package='nav2_lifecycle_manager',
+                                    executable='lifecycle_manager',
+                                    name='lifecycle_manager_navigation',
+                                    output='screen',
+                                    namespace=ns,
+                                    parameters=[{'use_sim_time': True},
+                                                {'autostart': True},
+                                                {'node_names': lifecycle_nodes}]
+                                ))
 
-        DeclareLaunchArgument(
-            'namespace', default_value='',
-            description='Top-level namespace'),
-
-        DeclareLaunchArgument(
-            'use_sim_time', default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-
-        DeclareLaunchArgument(
-            'autostart', default_value='true',
-            description='Automatically startup the nav2 stack'),
-
-        DeclareLaunchArgument(
-            'params_file',
-            default_value=os.path.join(bringup_dir, 'config', 'nav2_params_tug_1.yaml'),
-            description='Full path to the ROS2 parameters file to use'),
-
-        DeclareLaunchArgument(
-            'default_bt_xml_filename',
-            default_value=os.path.join(
-                get_package_share_directory('nav2_bt_navigator'),
-                'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
-            description='Full path to the behavior tree xml file to use'),
-
-        DeclareLaunchArgument(
-            'map_subscribe_transient_local', default_value='false',
-            description='Whether to set the map subscriber QoS to transient local'),
-
-        Node(
-            package='nav2_controller',
-            executable='controller_server',
-            output='screen',
-            namespace=ns,
-            parameters=[configured_params]),
-
-        Node(
-            package='nav2_planner',
-            executable='planner_server',
-            name='planner_server',
-            output='screen',
-            namespace=ns,
-            parameters=[configured_params]),
-
-        Node(
-            package='nav2_recoveries',
-            executable='recoveries_server',
-            name='recoveries_server',
-            output='screen',
-            namespace=ns,
-            parameters=[configured_params]),
-
-        Node(
-            package='nav2_bt_navigator',
-            executable='bt_navigator',
-            name='bt_navigator',
-            output='screen',
-            namespace=ns,
-            parameters=[configured_params]),
-
-        Node(
-            package='nav2_waypoint_follower',
-            executable='waypoint_follower',
-            name='waypoint_follower',
-            output='screen',
-            namespace=ns,
-            parameters=[configured_params]),
-
-        Node(
-            package='nav2_lifecycle_manager',
-            executable='lifecycle_manager',
-            name='lifecycle_manager_navigation',
-            output='screen',
-            namespace=ns,
-            parameters=[{'use_sim_time': use_sim_time},
-                        {'autostart': autostart},
-                        {'node_names': lifecycle_nodes}]),
-
-
-    ])
+    return LaunchDescription(
+        nav_nodes + lifecycle_manager_nodes
+    )
